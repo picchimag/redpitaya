9ede9a11a1d63b8eddbc96e9168117b3dd855a31
lorenzo <lorenzomgrn@gmail.com>
2026-03-13 15:58:45 -0700

    created subrepository

--- python_rp_old/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Red Pitaya control modules"""
-
-from .python_rp import redpitaya_dev
-from .python_rp import compute_coeff
--- python_rp_old/python_rp/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Red Pitaya control modules"""
-from .redpitaya_dev import redpitaya_dev
-from .compute_coeff import *
